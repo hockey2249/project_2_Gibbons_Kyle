8bea983ede668015c51c3823b3250bc30f451047
hockey2249 <kgibbons14@gmail.com>
2016-07-22 08:48:57 -0600

    Pushing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,2 @@
 require './models/pokemon_gym'
 require './models/pokemon_ad'
-
-# Pokemon.create({
-#     name: "Pokemon Gym: Union Station",
-#     address: "1701 Wynkoop St, Denver, CO 80202",
-#     phone_number: "3034332739",
-#     website: "www.unionstation.com"
-# })
-
-# Pokemon.create({
-#     name: "Pokemon Gym: Union Station",
-#     address: "1720 Wynkoop St, Denver, CO 80202",
-#     phone_number: "3034332739",
-#     website: "www.unionstation.com"
-# })
-
-# Pokemonad.create({
-#     pokemon_name: "Charizard",
-#     photo_url: "http://vignette4.wikia.nocookie.net/pokemon/images/e/e5/006Charizard_AG_anime.png/revision/latest?cb=20131213083244",
-#     address: "429 Acoma Street, Denver, CO",
-# })
-
